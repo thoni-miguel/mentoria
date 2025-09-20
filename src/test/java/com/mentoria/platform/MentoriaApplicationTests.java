9c93cb0b850180971fd6627453abf51f2ce9fa58
Thoni Araujo <thoni@Thonis-MacBook-Air.local>
2025-09-20 15:48:32 -0300

    first commit
    Simple first commit containing the initial spring boot setup

---
+++ src/test/java/com/mentoria/platform/MentoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentoria.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
